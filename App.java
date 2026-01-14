fcffc5dc1a11e47be60486bfbcb0129e3d56ba1b
Prajwal Wankhede <prajwalwankhede003@gmail.com>
2026-01-14 23:02:03 +0530

    Update App.java

--- App.java
+++ App.java
@@ -4,8 +4,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Maven!");
     }
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
 }
+
