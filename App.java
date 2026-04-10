4b10203635d4c7d1701e0f578543dee5f181225a
Prajwal Wankhede <prajwalwankhede003@gmail.com>
2026-04-10 20:15:30 +0530

    Update App.java

--- App.java
+++ App.java
@@ -3,7 +3,16 @@ package com.example;
 public class App {
     public static void main(String[] args) {
 
-        int unused = 10; // SonarQube will flag this
+        int unused = 10;
+
+        String password = "admin123";
+
+        int timeout = 5000;
+
+        try {
+            int x = 10 / 0;
+        } catch (Exception e) {
+        }
 
         System.out.println("Hello, Maven!");
     }
